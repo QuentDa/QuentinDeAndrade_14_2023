8ad61cbf8fc0ca94e68d36632c2be501721ccf79
Quentin De Andrade <deandradeq@gmail.com>
2023-06-04 21:07:12 +0200

    DataTable

--- src/pages/Employees/Employees.js
+++ src/pages/Employees/Employees.js
@@ -1,7 +1,10 @@
+import EmployeeTable from "../../components/Table/DataTable";
 export default function Employees() {
     return (
         <div className="employees">
             <h2>Employees</h2>
+
+            <EmployeeTable />
         </div>
     );
 }
